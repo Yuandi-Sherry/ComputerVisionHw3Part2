484c9d309c9b44949baa97d808198f5278e2e956
周远笛 <1094050886@qq.com>
2018-10-24 20:52:45 +0800

    第六幅图完成

--- CircleDetector.h
+++ CircleDetector.h
@@ -4,6 +4,8 @@
 using namespace std;
 using namespace cimg_library;
 #define M_PI 3.14159265358979323846
+int thresDev[7] = {0, 4,  5.5, 10, 10,30,1.5 };
+int stepDev[7] = {0, 100, 25, 25, 25 , 100,100};
 struct point {
 	int x;
 	int y;
@@ -25,44 +27,54 @@ private:
 	double dia;
 	CImg<unsigned char> img;
 	CImg<unsigned char> result;
+	CImg<unsigned char> edge;
 	vector<point> potentialCircle;
 	vector<point> selectedCircle;
 	clock_t  start, end;
 public:
 	CircleDetector(CImg<unsigned char> img, CImg<unsigned char> originalImg, int id) {
-		start = clock();
+		
+		cimg_forXY(edge, i, j) {
+			if (img(i, j) == 255) {
+				edge(i, j, 0, 0) = 255;
+				edge(i, j, 0, 1) = 255;
+				edge(i, j, 0, 2) = 255;
+			}
+			else {
+				edge(i, j, 0, 0) = 0;
+				edge(i, j, 0, 1) = 0;
+				edge(i, j, 0, 2) = 0;
+			}
+		}
 		this->result = originalImg;
 		this->img = img;
 		this->id = id;
 		this->width = img.width();
 		this->height = img.height();
-		maxR = min(width , height)/2; 
-		dia = sqrt(pow(height,2) + pow(width, 2))/2;
+		edge = CImg<unsigned char>(width, height, 1, 3);
+		maxR = min(width, height) / 2;
+		dia = sqrt(pow(height, 2) + pow(width, 2)) / 2;
 		step = maxR / 100;
-		//cout << "maxR " << maxR << endl; 
-		//cout << "dia " << dia << endl;
-		accumulation = CImg<unsigned char>(width, height, ceil(maxR/step) + 1,1);
+		accumulation = CImg<unsigned char>(width, height, ceil(maxR / step) + 1, 1);
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
-				for (int k = 0; k < maxR; k = k+step) {
-					accumulation(i,j,k/ step,0) = 0;
+				for (int k = 0; k < maxR; k = k + step) {
+					accumulation(i, j, k / step, 0) = 0;
 				}
 			}
 		}
+		start = clock();
 		vote();
-		filterThershold(maxR /30);
+		filterThershold(maxR /1.3);
 		filter();
-		end = clock();
 		drawCircles();
+		end = clock();
 	}
 
 	void vote() {
-		string t = to_string(id) + "edge_test1.bmp";
+		string t = to_string(id) + "_Iedge.bmp";
 		const char * temp = t.c_str();
 		img.save(temp);
-		//result.display();
-		//img.display();
-		cout << "vote" << endl;
 		// 遍历圆周上点
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
@@ -101,7 +113,6 @@ public:
 			if (jumpOut)
 				break;
  		}
-		cout << "maxRadius "<< maxRadius << endl;
 
 		for (int r = 0; r < maxR; r = r + step) {
 			for (int i = r; i < width - r; i++) {
@@ -129,7 +140,6 @@ public:
 					}
 				}
 			}
-			cout << r << endl;
 		}
 
 	}
@@ -159,104 +169,55 @@ public:
 				i++;
 			}
 		}
-		//cout << "count in filter " << count << endl;
-	}
-
-	void findLocalMaximums() {
-		//cout << "findLocalMaximums" << endl;
-
-		for (int i = 0; i < potentialCircle.size(); ) {
-			bool iChange = false;
-			for (int j = i + 1; j < potentialCircle.size(); ) {
-				// 判断半径
-				
-					if (potentialCircle[i].r < 0.5 * potentialCircle[j].r) {
-						potentialCircle.erase(potentialCircle.begin() + i);
-						iChange = true;
-						break;
-					}
-					else if (potentialCircle[j].r < 0.5 * potentialCircle[i].r) {
-						potentialCircle.erase(potentialCircle.begin() + j);
-						continue;
-					}
-				
-				j++;
-			}
-			if (!iChange) {
-				i++;
-			}
-
-		}
-
-		for (int i = 0; i < potentialCircle.size(); ) {
-			bool iChange = false;
-			for (int j = i + 1; j < potentialCircle.size(); ) {
-				// 判断半径
-				if (pow(potentialCircle[i].x - potentialCircle[j].x, 2) + pow(potentialCircle[i].y - potentialCircle[j].y, 2) < pow(potentialCircle[i].r + potentialCircle[j].r, 2)*0.9) {
-					if (potentialCircle[i].r < 0.5 * potentialCircle[j].r) {
-						potentialCircle.erase(potentialCircle.begin() + i);
-						iChange = true;
-						break;
-					}
-					else if (potentialCircle[j].r < 0.5 * potentialCircle[i].r) {
-						potentialCircle.erase(potentialCircle.begin() + j);
-						continue;
-					}
-				}
-				j++;
-			}
-			if (!iChange) {
-				i++;
-			}
-			
-		}
-
-		for (int i = 0; i < potentialCircle.size(); ) {
-			bool iChange = false;
-			for (int j = i + 1; j < potentialCircle.size(); ) {
-				if (pow(potentialCircle[i].x - potentialCircle[j].x, 2) + pow(potentialCircle[i].y - potentialCircle[j].y, 2) < pow(potentialCircle[i].r + potentialCircle[j].r, 2) ) {
-					if (accumulation(potentialCircle[i].x, potentialCircle[i].y, potentialCircle[i].r) < accumulation(potentialCircle[j].x, potentialCircle[j].y, potentialCircle[j].r)) {
-						potentialCircle.erase(potentialCircle.begin() + i);
-						iChange = true;
-						break;
-					}
-					else {
-						potentialCircle.erase(potentialCircle.begin() + j);
-						continue;
-					}
-				}
-				j++;
-			}
-			if (!iChange) {
-				i++;
-			}
-
-		}
-		cout << potentialCircle.size() << endl;
 	}
 
 	void drawCircles() {
 		const double red[] = { 255, 0, 0 };
+		const double green[] = { 0, 255, 0 };
 		cout << "drawCircles" << potentialCircle.size() << endl;
 		for (int i = 0; i < potentialCircle.size(); i++) {
 			for (int theta = 0; theta < 360; theta++) {
 				int x = potentialCircle[i].x + potentialCircle[i].r * cos(theta*M_PI / 180);
 				int y = potentialCircle[i].y + potentialCircle[i].r * sin(theta*M_PI / 180);
 				if (x >= -3 && x < width && y >= -3 && y < height) {
-					result.draw_circle(x + 3, y + 3, 2, red);
+					result.draw_circle(x + 3, y + 3, 2, green);
+					edge.draw_circle(x + 3, y + 3, 2, green);
 				}
 				
 			}
 		}
-		string name = to_string(id) + "final_test1.bmp";
-		const char* input = name.c_str();
-		//result.display();
-		result.save(input);
+		string name_origin = to_string(id) + "_origin_I2.bmp";
+		const char* input_origin = name_origin.c_str();
+		result.save(input_origin);
+		string name_edge = to_string(id) + "_edge_I2.bmp";
+		const char* input_edge = name_edge.c_str();
+		edge.save(input_edge);
+
+
 		cout << "finish drawCircles" << endl;
 		cout << "end" << end << endl;
 		cout << "start" << start << endl;
 		cout << "CLK_TCK" << CLK_TCK << endl;
-		cout << "Hough Transform用时：" << (end - start) / CLK_TCK << endl;
+		cout << "Hough Transform用时：" << (double)(end - start) / CLK_TCK << endl;
+
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if (edge(i, j, 0, 2) == 0
+					&& edge(i, j, 0, 1) == 255
+					&& edge(i, j, 0, 0) == 0) {
+					if (img(i, j) == 255) {
+						edge(i, j, 0, 0) = 255;
+						edge(i, j, 0, 1) = 0;
+						edge(i, j, 0, 2) = 0;
+					}
+				}
+			}
+		}
+
+		string t1 = to_string(id) + "-I3.bmp";
+		const char * temp1 = t1.c_str();
+		edge.save(temp1);
+		
 	}
 
 };
--- main.cpp
+++ main.cpp
@@ -7,14 +7,17 @@ using namespace cimg_library;
 using namespace std;
 int main()
 {
-	for (int i = 5; i < 6; i++) {
+	double percentage[7] = {0, 0.9, 0.8, 0.8, 0.8, 0,8, };
+	int thres[7] = {0, 100, 200, 200,300, 300, }
+
+	for (int i = 6; i < 7; i++) {
 		string name = to_string(i) + ".bmp";
 		const char* input = name.c_str();
 		CImg<unsigned char> originalImg(input);
 		string edge_origin = to_string(i) + "edge_test1.bmp";
 		input = edge_origin.c_str();
 		CImg<unsigned char> edgeImg(input); 
-		canny answer = canny(originalImg, 0.8, 300);
+		canny answer = canny(originalImg, 0.7, 60);
 		CircleDetector houghTransform = CircleDetector(edgeImg, originalImg, i);
 	}
 	/*for (int i = 1; i <= 6; i++) {
